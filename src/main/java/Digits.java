7b7d4907990eae255fd4a1312d5880eb1aee06d0
conerthecone894 <143015307+conerthecone894@users.noreply.github.com>
2024-04-16 06:42:57 +0000

    Update Digits.java
    attempt 1.

--- src/main/java/Digits.java
+++ src/main/java/Digits.java
@@ -6,14 +6,34 @@ public class Digits
 	private ArrayList<Integer> digitList;
 
 	public Digits(int num)
-	{ /* to be implemented in part (a) */ 
-	    
+	{ /* to be implemented in part (a) */
+		digitList = new ArrayList<Integer>();
+		
+	    	String x = Integer.toString(num);
+		int y = num;
+
+		for(int i = x.length()-1; i >= 0; i--){
+			digitList.add(y%10);
+			y = y/10;
+		}
 	}
 
 	public boolean isStrictlyIncreasing()
 	{ /* to be implemented in part (b) */
-		
+		if(num%10 = num){
+			return true;
+		}
 
+		String x = Integer.toString(num);
+		for(int i = 0; i < x.length()-1; i++){
+			int a = num%10;
+			int b = (num/10)%10;
+
+			if(b > a)
+				return false;
+		}
+
+		return true;
 	}
 	
 	public String toString()
